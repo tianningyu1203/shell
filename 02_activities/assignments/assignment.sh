ee479a03b17cde03d74f48e4027d4d1ffa57c1ef
tianningyu1203 <tianning.yu@mail.utoronto.ca>
2025-01-14 15:14:02 -0500

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -36,6 +36,7 @@ cp ./data/raw/*server*.log ./data/processed/server_logs/
 cp ./data/raw/*user*.log ./data/processed/user_logs
 cp ./data/raw/*event*.log ./data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 
 rt -rf ./data
 # rt -rf ./data  # This was corrected from rm -rf ./data. The change from coworker-change branch was a typo.
