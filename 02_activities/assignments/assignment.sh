749734083b158dee9fabc0ddc3046d5d9dbd7697
Tianning Yu <tianning.yu@mail.utoronto.ca>
2025-01-13 23:01:14 -0500

    copy all server log files into the server_log directory

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,7 +30,7 @@ ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 mkdir -p ./data/processed/{server_logs,user_logs,event_logs}
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+cp ./data/raw/*server*.log ./data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
